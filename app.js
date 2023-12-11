87f1bf29feb7bcd6bb6e9a7dc40dea11ab54550d
shakirat <shakiratganiyy@gmail.com>
2023-12-11 08:25:55 +0100

    Updated the app

--- app.js
+++ app.js
@@ -1,55 +1,22 @@
-const shows = document.querySelector(".shows");
 const likes = document.querySelector(".likes");
-
 function Movie(image, title, category) {
   this.image = image;
   this.title = title;
   this.category = category;
-  this.categoryType = categorytype;
-  this.likes = likes;
+  this.likes = 0;
 }
 
 const movies = [
-  new Movie(
-    "./images/oblivion.jpg",
-    "Oblivion",
-    "Science Fiction",
-    "Action",
-    0
-  ),
-  new Movie("./images/ribelle.jpg", "Ribelle", "Science Fiction", "Shows", 0),
-  new Movie(
-    "./images/wolverine.jpg",
-    "Wolverine",
-    "Science Fiction",
-    "Action",
-    0
-  ),
-  new Movie("./images/dexter.jpg", "Dexter", "Science Fiction", "Action", 0),
-  new Movie(
-    "./images/incredibles.jpg",
-    "The Incredibles",
-    "Science Fiction",
-    "Shows",
-    0
-  ),
-  new Movie(
-    "./images/man of steel.jpg",
-    "Man of Steel",
-    "Science Fiction",
-    "Action",
-    0
-  ),
-  new Movie("./images/gatsby.jpeg", "Gatsby", "Science Fiction", "Shows", 0),
-  new Movie("./images/drive.jpg", "Drive", "Science Fiction", "Action", 0),
-  new Movie(
-    "./images/ironman3.jpeg",
-    "Iron Man 3",
-    "Science Fiction",
-    "Action",
-    0
-  ),
-  new Movie("./images/django.jpeg", "Django", "Science Fiction", "Action", 0),
+  new Movie("./images/oblivion.jpg", "Oblivion", "Action"),
+  new Movie("./images/ribelle.jpg", "Ribelle", "Shows"),
+  new Movie("./images/wolverine.jpg", "Wolverine", "Action"),
+  new Movie("./images/dexter.jpg", "Dexter", "Action"),
+  new Movie("./images/incredibles.jpg", "The Incredibles", "Shows"),
+  new Movie("./images/man of steel.jpg", "Man of Steel", "Action"),
+  new Movie("./images/gatsby.jpeg", "Gatsby", "Shows"),
+  new Movie("./images/drive.jpg", "Drive", "Action"),
+  new Movie("./images/ironman3.jpeg", "Iron Man 3", "Action"),
+  new Movie("./images/django.jpeg", "Django", "Action"),
 ];
 
 const newMovieContainer = document.querySelector(".main-movies");
@@ -57,14 +24,18 @@ const newMovieContainer = document.querySelector(".main-movies");
 function displayMovies(movieList) {
   newMovieContainer.innerHTML = "";
   movieList.forEach((movie) => {
+    const likeClass = movie.likes > 0 ? "liked" : "";
+
     const newDiv = document.createElement("div");
     newDiv.classList.add("movie-container");
     newDiv.innerHTML = `<div class="movie-image">
       <img src="${movie.image}" alt="${movie.title}" />
     </div>
     <div class="movie-text">
+    <div>
       <div class="title">${movie.title}</div>
-      <p class="category">${movie.category}</p>
+      <p class="category">${movie.category}</p></div>
+     <button class="like-button"><i class="fa-regular fa-heart ${likeClass}"></i></button>
     </div>`;
     newMovieContainer.appendChild(newDiv);
   });
@@ -79,6 +50,7 @@ function searchMovies() {
   );
   displayMovies(updatedMovies);
 }
+
 const searchInput = document.querySelector(".search input");
 searchInput.addEventListener("input", searchMovies);
 searchInput.addEventListener("keydown", function (event) {
@@ -89,22 +61,25 @@ searchInput.addEventListener("keydown", function (event) {
 
 function filterMoviesByCategory(category) {
   const filteredMovies = movies.filter(
-    (movie) => movie.category.toLowerCase() === category.toLowerCase()
+    (movie) =>
+      category === "all" ||
+      movie.category.toLowerCase() === category.toLowerCase()
   );
   displayMovies(filteredMovies);
 }
 
-// function addLike(movie) {
-//   movie.likes++;
-// }
+const likeButtons = document.querySelectorAll(".like-button");
+likeButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    toggleLike(movies[index]);
+    displayMovies(movies);
+  });
+});
 
-// const likeButtons = document.querySelectorAll(".like-button");
-// likeButtons.forEach((button, index) => {
-//   button.addEventListener("click", () => {
-//     addLike(movies[index]);
-//     displayMovies(movies);
-//   });
-// });
+function toggleLike(movie) {
+  movie.likes = 1 - movie.likes;
+}
+displayMovies(movies);
 
 const all = document.querySelector(".all");
 all.addEventListener("click", () => {
@@ -114,4 +89,9 @@ all.addEventListener("click", () => {
 const action = document.querySelector(".action");
 action.addEventListener("click", () => {
   filterMoviesByCategory("Action");
+});
+
+const shows = document.querySelector(".shows");
+shows.addEventListener("click", () => {
+  filterMoviesByCategory("Shows");
 });
